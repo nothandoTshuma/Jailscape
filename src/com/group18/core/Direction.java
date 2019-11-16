b8d5158c1302a944b3697d512b2251dde3d6b098
Daniel <dturato@hotmail.com>
2019-11-16 09:36:11 +0000

    Added entity class & direction enum

---
+++ src/com/group18/core/Direction.java
@@ -0,0 +1,12 @@
+package com.group18.core;
+
+/**
+ * Enum representing the direction in which entities can move in the game
+ * @author danielturato nothandotshuma riyagupta
+ */
+public enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
