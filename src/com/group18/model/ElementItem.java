ed206370901fe4d6b0811cd8a73dcb9e87515efa
RIYA <guptar1501@yahoo.com>
2019-11-22 13:39:48 +0000

    Enums, Element and TokenDoor classes

---
+++ src/com/group18/model/ElementItem.java
@@ -0,0 +1,10 @@
+package com.group18.model;
+
+/**
+ * Represents different element items
+ * @author Riya Gupta
+ */
+public enum ElementItem implements Collectable {
+    FLIPPER,
+    FIRE_BOOTS;
+}
