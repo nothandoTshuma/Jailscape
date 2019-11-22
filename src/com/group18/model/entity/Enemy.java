0052936ef788690f8abefc3ab0cc0ad4099f930a
Nothando <925870@swansea.ac.uk>
2019-11-22 14:01:14 +0000

    Worked on Enemy class.

--- src/com/group18/model/entity/Enemy.java
+++ src/com/group18/model/entity/Enemy.java
@@ -1,4 +1,19 @@
 package com.group18.model.entity;
 
-public class Enemy extends Entity {
+import com.group18.model.Direction;
+
+/**
+ * Designs the behaviours for the Enemy.
+ * @author nothandotshuma
+ */
+public abstract class Enemy extends Entity {
+
+    /**
+     * Abstract method to work out which direction the enemy should take.
+     * @param user
+     * @param board
+     * @return Direction
+     */
+    public abstract Direction getNextDirection(User user, Board board);
+
 }
