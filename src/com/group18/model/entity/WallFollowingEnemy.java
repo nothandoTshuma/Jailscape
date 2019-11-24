a7b116264f6d5ae4b54a1a1be7c76e10fd86fc22
danielturato <dturato@hotmail.com>
2019-11-24 21:15:18 +0000

    Updated WallFollowingEnemy

--- src/com/group18/model/entity/WallFollowingEnemy.java
+++ src/com/group18/model/entity/WallFollowingEnemy.java
@@ -2,7 +2,10 @@ package com.group18.model.entity;
 
 import com.group18.model.Direction;
 import com.group18.model.Level;
+import com.group18.model.cell.Cell;
+import com.group18.model.cell.Wall;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -25,21 +28,163 @@ public class WallFollowingEnemy extends Enemy {
     @Override
     public Direction getNextDirection(User user, Level level) {
         // Collect all valid possible directions this enemy can move in
-        List<Direction> validDirections =
-                Arrays.stream(Direction.values())
-                        .filter(direction -> level.validMove(this, direction))
-                        .filter(direction -> direction != IDLE)
-                        .collect(Collectors.toList());
+        List<Direction> validDirections = getValidDirections(level);
 
+        // Attempt to get a valid direction, which is only possible if there
+        // were found to be 2 valid directions
+        Direction possibleDirection = getValidDirection(validDirections);
+        if (possibleDirection != null) {
+            return possibleDirection;
+        }
+
+        // Here, the enemy must be at a crossroad, but we must eliminate those directions which
+        // would take this enemy away from a wall.
+        eliminateInvalidDirections(validDirections, level);
+
+        // If valid directions still contain 3 possible valid directions
+        // the enemy must be at a 4 way single path crossroads in which they can choose any path.
+        if (validDirections.size() == 3) {
+            Random random = new Random();
+            return validDirections.get(random.nextInt(3));
+        }
+
+        // However, if valid directions is below 3 then we can re-calculate a valid position
+        // from those left.
+        return getValidDirection(validDirections);
+    }
+
+    /**
+     * Get a valid direction out of a list of possible valid directions
+     * @param validDirections Possible valid directions
+     * @return A possible direction or null if no possible direction was found
+     */
+    private Direction getValidDirection(List<Direction> validDirections) {
         // If there were no valid directions, this enemy is stuck
         if (validDirections.size() == 0) {
             return IDLE;
         }
 
-        // If this enemy is not stuck, choose a random valid direction for them to move in
-        Random random = new Random();
-        int randomDirectionIdx = random.nextInt(validDirections.size());
+        //If there's only 1 valid direction, then they must be at a dead end and turn around
+        if (validDirections.size() == 1) {
+            return validDirections.get(0);
+        }
 
-        return validDirections.get(randomDirectionIdx);
+        //If there's 2 valid directions, they could be in multiple different positions
+        if (validDirections.size() == 2) {
+            //A single path surrounded by walls
+            // in which then they should just continue in their previous direction
+            if (validDirections.containsAll(Arrays.asList(UP, DOWN)) ||
+                    validDirections.containsAll(Arrays.asList(LEFT, RIGHT))) {
+
+                return this.getDirection();
+            }
+
+            //A corner in which valid directions maybe (UP, RIGHT), (DOWN, RIGHT), (LEFT, DOWN), (LEFT, UP),
+            //here the enemy should try and move in the direction that is not opposite their previous move
+            Direction oppositeDirection = reverseDirection(this.getDirection());
+            validDirections.remove(oppositeDirection);
+
+            return validDirections.get(0);
+        }
+
+        return null;
     }
+
+    /**
+     * Eliminate directions which aren't deemed possible
+     * @param validDirections A list of possible valid directions
+     * @param level The level this enemy is currently on
+     */
+    private void eliminateInvalidDirections(List<Direction> validDirections, Level level) {
+        Point currentPosition = this.getCurrentCell().getPosition();
+
+        // Loop through each possible valid direction to move in
+        for (Direction direction : validDirections) {
+            int x = (int) currentPosition.getX();
+            int y = (int) currentPosition.getY();
+
+            switch (direction) {
+                case LEFT:
+                    x--;
+                    break;
+                case RIGHT:
+                    x++;
+                    break;
+                case UP:
+                    y--;
+                    break;
+                case DOWN:
+                    y++;
+                    break;
+                default:
+                    break;
+            }
+
+            //Get the adjacent cells from that possible new position (removing old position)
+            List<Cell> adjacentCells = level.getAdjacentCells(new Point(x, y));
+            adjacentCells.remove(this.getCurrentCell());
+
+            // Now loop through the adjacent cells and check there's at least one wall adjacent to the
+            // possible new position
+            boolean isValidDirection = false;
+            for (Cell cell : adjacentCells) {
+                if (cell instanceof Wall) {
+                    isValidDirection = true;
+                }
+            }
+
+            // If the possible direction lands them in a cell with no adjacent wall cells,
+            // then this direction is no longer valid.
+            if (!(isValidDirection)) {
+                validDirections.remove(direction);
+            }
+        }
+    }
+
+    /**
+     * Generate a starter list of valid directions for this enemy to move in
+     * @param level The level this enemy is on
+     * @return A starter list of possible valid directions
+     */
+    private List<Direction> getValidDirections(Level level) {
+        return Arrays.stream(Direction.values())
+                .filter(direction -> level.validMove(this, direction))
+                .filter(direction -> direction != IDLE)
+                .collect(Collectors.toList());
+    }
+
+
+    /**
+     * Reverse the direction of a particular direction
+     * @param previousDirection The direction to be reversed
+     * @return The reversed direction
+     */
+    private Direction reverseDirection(Direction previousDirection) {
+        Direction newDirection;
+
+        switch (previousDirection) {
+            case UP:
+                newDirection = DOWN;
+                break;
+            case DOWN:
+                newDirection = UP;
+                break;
+            case LEFT:
+                newDirection = RIGHT;
+                break;
+            case RIGHT:
+                newDirection = LEFT;
+                break;
+            default:
+                newDirection = IDLE;
+                break;
+        }
+
+        return newDirection;
+    }
+
 }
+
+
+
+
