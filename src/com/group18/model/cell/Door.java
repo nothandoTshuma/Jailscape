997a9fc476d971e7d6312dfc7c1c60802826161c
Nothando <925870@swansea.ac.uk>
2019-11-25 16:17:24 +0000

    Added Goal, Teleporter & Custom exceptions

--- src/com/group18/model/cell/Door.java
+++ src/com/group18/model/cell/Door.java
@@ -1,4 +1,20 @@
 package com.group18.model.cell;
 
+import com.group18.model.entity.User;
+
+/**
+ * Interface class implemented by any class which uses the door.
+ * @ author nothandotshuma
+ */
 public interface Door {
+
+    /**
+     * This method checks if the user has the right key to open the door and if they do, it
+     * returns True, otherwise it returns False.
+     * @returns True, otherwise it returns False.
+     */
+    public boolean canOpen(User user);
+
+
 }
+
