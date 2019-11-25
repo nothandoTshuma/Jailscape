c012a2cc5658eaf619147cbe1efef19974b006c1
Nothando <925870@swansea.ac.uk>
2019-11-25 16:17:24 +0000

    Added Goal, Teleporter & Custom exceptions

--- src/com/group18/exception/InvalidMoveException.java
+++ src/com/group18/exception/InvalidMoveException.java
@@ -1,5 +1,25 @@
 package com.group18.exception;
 
+/**
+ * An exception thrown when an entity attempts to make an invalid move
+ * @author danielturato nothandotshuma riyagupta
+ */
 public class InvalidMoveException extends Exception {
-    //TODO:drt - Complete this class
+
+    /**
+     * Creates a new InvalidMoveException
+     * @param message The message associated with the exception
+     */
+    public InvalidMoveException(String message) {
+        super(message);
+    }
+
+    /**
+     * Get this exceptions message
+     * @return The message
+     */
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
 }
