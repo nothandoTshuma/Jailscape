6daaa365107f0429c198673d7a7c8c1972079a9a
RIYA <guptar1501@yahoo.com>
2019-11-25 14:23:39 +0000

    Enums, Element and TokenDoor classes

---
+++ src/com/group18/model/cell/ColourDoor.java
@@ -0,0 +1,38 @@
+package com.group18.model.cell;
+
+import com.group18.model.Actionable;
+import com.group18.model.Colour;
+import com.group18.model.entity.Entity;
+import com.group18.model.entity.User;
+
+/**
+ * Represents the coloured door which requires same coloured key same to be opened.
+ * @author RIYA GUPTA
+ * */
+public class ColourDoor extends Wall implements Actionable {
+     /**
+     * represents the colour of the door
+     */
+    private final Colour colour;
+
+    /**
+     * represents constructor that takes the colour of the door and colour of key needed to open the door.
+     * @param colour returns the colour needed to open the door.
+     */
+    public ColourDoor(Colour colour) {
+        this.colour = colour;
+    }
+    /**
+     * represents whether user can open the door or not
+     * @param user checks if it can open the door
+     * @return boolean value suggesting if the door can be opened.
+     */
+    public boolean canOpen(User user)
+    {
+
+        return user.hasKey(colour);
+    }
+
+    public void toggleAction(Entity entity) {
+    }
+}
